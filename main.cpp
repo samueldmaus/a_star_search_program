933cde1e9cc5478be1ca4ef185e208dda3ba785b
samueldmaus <samueldmaus@gmail.com>
2020-11-02 21:23:19 -0600

    added heuristic function to determine distance from end and added AddToOpen function to add nodes to open_list vector

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <numeric>
 
 using namespace std;
 
@@ -14,7 +15,23 @@ vector<vector<int> > board = {{0, 1, 0, 0, 0, 0},
 {0, 0, 0, 0, 1, 0}};
 */
 
-enum class State {kNothing, kObstacle};
+enum class State {kNothing, kObstacle, kClosed};
+
+// Heuristic function to determine how far away a point is from the finish
+int Heuristic(int x_1, int x_2, int y_1, int y_2)
+{
+    return (abs(x_2 - x_1) + abs(y_2 - y_1));
+}
+
+// function to add nodes to the open vector
+void AddToOpen(int x, int y, int g, int h, vector<vector<int> > &open_list, vector<vector<State> > &grid)
+{
+    // add the node to the open_list vector
+    open_list.push_back(vector<int>{x, y, g, h});
+
+    // close that node so it doesn't get added again
+    grid[x][y] = State::kClosed;
+}
 
 // parse through each line of the .txt file
 vector<State> ParseLine(string &line)
