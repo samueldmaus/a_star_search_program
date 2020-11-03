651244cc3af37f3d328f9e12b80dcb2c1eda9bc2
samueldmaus <samueldmaus@gmail.com>
2020-11-02 19:33:40 -0600

    added function to print the maze/board to the console

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using namespace std;
+
+vector<vector<int> > board = {{0, 1, 0, 0, 0, 0},
+{0, 1, 0, 0, 0, 0},
+{0, 1, 0, 0, 0, 0},
+{0, 1, 0, 0, 0, 0},
+{0, 0, 0, 0, 1, 0}};
+
+void PrintTheBoard(vector<vector<int> > &v)
+{
+    for(vector<int> i : v)
+    {
+        for(int j : i)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    PrintTheBoard(board);
+}
 
